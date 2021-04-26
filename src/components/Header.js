04091dea30b128f3d4c0e00f091459334b0a8761
Poonam Thorup <poonam.thorup@gmail.com>
2021-04-26 13:25:14 +0200

    minor changes in header and home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <header>
-            <nav>
-                <Link to='/'>Home</Link>
-                <Link to='/category'>Types of Ponies</Link>
-                <Link to='/favourites'>Favourites</Link>
-                <Link to='/songs'>Songs</Link>
-                <Link to='/style-pony'>Style a Pony</Link>
+        <header style={{ border: '2px solid pink' }}>
+            <h1>My Little Pony</h1>
+            <nav style={{ textAlign: 'center' }}>
+                <Link to='/'>Home </Link>
+                <Link to='/category'>Types of Ponies </Link>
+                <Link to='/favourites'>Favourites </Link>
+                <Link to='/songs'>Songs </Link>
+                <Link to='/style-pony'>Style a Pony </Link>
             </nav>
         </header>
     )
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from 'react'
 
 const Home = () => {
-    return ( <div>This is home</div> );
+    return (
+        <div>
+            <p>
+                Welcome to my little pony app where you are learn about your
+                favourite pony and do a bunch of other stuff
+            </p>
+        </div>
+    )
 }
- 
-export default Home;
+
+export default Home
